4fea28c1637b071cc4f36d3ce3c3e76c80d6409f
root <root@tinker-debuger.maas>
2020-11-30 06:52:22 +0000

    update

--- Case3/OpenWhisk/Node/action/create_action.sh
+++ Case3/OpenWhisk/Node/action/create_action.sh
@@ -1,1 +1,4 @@
-wsk -i action update /guest/demo/hello hello.js --web true
+wsk -i action update hello-node hello.js
+
+
+wsk -i action update hello-java hello.jar --main Hello --docker tinker.siat.ac.cn/openwhisk/java8action
