47f87fd20c853fd0748872e1548fa77314287ab4
tinker <whoszus@yahoo.com>
2020-12-17 11:35:46 +0800

    update

--- DIC/MachineLearning/classification of text/action-update.sh
+++ DIC/MachineLearning/classification of text/action-update.sh
@@ -1,4 +1,4 @@
-docker run --rm -v "$PWD:/tmp" ibmfunctions/action-python-v3.7 bash -c \
+docker run --rm -v "$PWD:/tmp" tinker.siat.ac.cn/tinker/siat-serverless-ow-py:t.1.0 bash -c \
 "cd /tmp && virtualenv virtualenv && source virtualenv/bin/activate && pip install -r requirement.txt" &&
 zip -r env.zip ./*.py virtualenv &&
-wsk -i  action update  TextClassification  env.zip --kind python:3
+wsk -i  action update  TextClassification  env.zip --docker tinker.siat.ac.cn/tinker/siat-serverless-ow-py:t.1.0
