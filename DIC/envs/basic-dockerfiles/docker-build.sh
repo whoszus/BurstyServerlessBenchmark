301098086009973846c7e3821f1d33ed7b91ae97
tinker.siat <whosuzs@yahoo.com>
2021-01-04 11:25:01 +0800

    UPDATE

--- DIC/envs/basic-dockerfiles/docker-build.sh
+++ DIC/envs/basic-dockerfiles/docker-build.sh
@@ -1,1 +1,2 @@
-docker build . -f Dockerfile-py-ml-basic-alpine -t tinker.siat.ac.cn/tinker/siat-serverless-py-basic:1.0.0
+docker build . -f Dockerfile-py-ml-basic-alpine -t tinker.siat.ac.cn/tinker/siat-serverless-py-basic:1.0.0 &&
+docker push tinker.siat.ac.cn/tinker/siat-serverless-py-basic:1.0.0
