35f32b3d6865a7043d703f9e97df0a4b2e5c0aed
tinker.siat <whosuzs@yahoo.com>
2021-01-06 16:38:35 +0800

    update version

--- DIC/MachineLearning/code-creating/openwhisk-function-generate-ml.py
+++ DIC/MachineLearning/code-creating/openwhisk-function-generate-ml.py
@@ -6,8 +6,16 @@ conf.read("Settings_values.cfg")
 docker_file_path = conf['BBServerless']['docker_file_path']
 version = conf['BBServerless']['version']
 functions_dir = '../Inference-of-ml-openwhisk/'
-model_path = '../../envs/Runtimes/ml-inference-with-data-RT/models/'
+
 tpl_py = './code-creating/openwhisk_function_tpl_py.py'
+model_path = conf['BBServerless']['model_path']
+
+data_path = '../../envs/Runtimes/ml-inference-with-data-RT/testData/'
+basic_docker_file = 'templates/openwhisk-python3.Dockerfile'
+
+conf = ConfigParser()
+conf.read("Settings_values.cfg")
+abs_dir = conf['BBServerless']['abs_dir']
 
 
 def main():
@@ -22,6 +30,45 @@ def main():
         generate_func(func_abs_path, m)
         generate_invoke_shell(func_abs_path, m)
         generate_action_update_shell(func_abs_path, m)
+
+    # 运行时环境
+    generate_rt()
+    openfaas_runtime_create()
+
+
+def generate_rt():
+    global datas, models
+    for root, dirs, files in os.walk(model_path, topdown=False):
+        models = files
+
+    for root, dirs, files in os.walk(data_path, topdown=False):
+        datas = files
+
+    # copy models
+    for m in models:
+        model = 'models/' + m
+        with open(basic_docker_file, 'r') as basic:
+            commands = basic.readline()
+
+        new_docker_file_name = docker_file_path + 'openwhisk-python3.Dockerfile-' + m
+        copy_model_command = '\nCOPY {model} /model/'.format(model=model)
+        commands += copy_model_command
+
+        with open(new_docker_file_name, 'w') as docker_file:
+            docker_file.write(commands)
+            docker_file.close()
+        basic.close()
+
+    # copy Platform-Testing data
+    for d in datas:
+        model_data_path = 'testData/' + d
+        if models.__contains__(d):
+            copy_model_command = '\nCOPY {data} /data/'.format(data=model_data_path)
+            new_docker_file_name = docker_file_path + 'openwhisk-python3.Dockerfile-' + d
+            with open(new_docker_file_name, 'a+') as docker_file:
+                docker_file.write(copy_model_command)
+                docker_file.close()
+            basic.close()
 
 
 def get_sub_pack(name):
@@ -76,6 +123,27 @@ def generate_action_update_shell(path, name):
                                                                                 tag=docker_tag)
     with open(path, 'w') as func:
         func.write(s)
+
+
+def openfaas_runtime_create():
+    global docker_files
+    for root, dirs, files in os.walk(docker_file_path, topdown=False):
+        docker_files = files
+    for f in docker_files:
+        # df = docker_file_path + f
+        name = f.split('-')[1].lower()
+        docker_tag = 'tinker.siat.ac.cn/tinker/siat-serverless-OF-{name}:{version}'.format(name=name,
+                                                                                           version=version)
+        run_command = 'cd {abs} && docker build . -f {dockerfile} -t {tag}'.format(abs=abs_dir, dockerfile=f,
+                                                                                   tag=docker_tag)
+
+        upload_command = 'docker push {tag}'.format(tag=docker_tag)
+        # p = subprocess.Popen(run_command, stdout=subprocess.PIPE, shell=True)
+        # (output, err) = p.communicate()
+        # p_status = p.wait()
+        # print("Command output: " + run_command)
+        os.popen(run_command).read()
+        os.popen(upload_command).read()
 
 
 main()
--- DIC/MachineLearning/code-creating/openwhisk-run-time-create.py
+++ DIC/MachineLearning/code-creating/openwhisk-run-time-create.py
@@ -5,7 +5,7 @@ conf = ConfigParser()
 conf.read("Settings_values.cfg")
 docker_file_path = conf['BBServerless']['docker_file_path']
 version = conf['BBServerless']['version']
-abs_dir = '/home/serverless-remote/DIC/envs/Runtimes/ml-inference-with-data-RT'
+abs_dir = conf['BBServerless']['abs_dir']
 
 
 def runtime_create():
@@ -29,22 +29,3 @@ def runtime_create():
         os.popen(upload_command).read()
 
 
-def openfaas_runtime_create():
-    global docker_files
-    for root, dirs, files in os.walk(docker_file_path, topdown=False):
-        docker_files = files
-    for f in docker_files:
-        # df = docker_file_path + f
-        name = f.split('-')[1].lower()
-        docker_tag = 'tinker.siat.ac.cn/tinker/siat-serverless-OF-{name}:{version}'.format(name=name,
-                                                                                        version=version)
-        run_command = 'cd {abs} && docker build . -f {dockerfile} -t {tag}'.format(abs=abs_dir, dockerfile=f,
-                                                                                   tag=docker_tag)
-
-        upload_command = 'docker push {tag}'.format(tag=docker_tag)
-        # p = subprocess.Popen(run_command, stdout=subprocess.PIPE, shell=True)
-        # (output, err) = p.communicate()
-        # p_status = p.wait()
-        # print("Command output: " + run_command)
-        os.popen(run_command).read()
-        os.popen(upload_command).read()
