aa481913460c9856e6ecb382196400a19a509bc9
tinker.siat <whosuzs@yahoo.com>
2021-01-13 21:26:03 +0800

    update version

---
+++ DIC/Stream/kubeless/kubeless-invoke.sh
@@ -0,0 +1,3 @@
+docker build . -f customized-demo.Dockerfile -t  tinker.siat.ac.cn/kubeless/demo:1.0.0 &&\
+docker push  tinker.siat.ac.cn/kubeless/demo:1.0.0 &&\
+kubeless function deploy --runtime-image tinker.siat.ac.cn/kubeless/demo:1.0.0 --from-file ./handler.py --handler handler.handler --runtime python3.7 demo
--- DIC/Stream/openfaas/stream/handler.py
+++ DIC/Stream/openfaas/stream/handler.py
@@ -1,17 +1,13 @@
-from collections import Counter
 import time
-import random
+from collections import Counter
 from urllib import request
+import random
 
-def main(args):
-
+def handle():
     startTime = time.time()
     url = 'http://172.16.101.131:10000/files/stream_data/{i}'.format(i=random.randint(0, 299))
     filedata = request.urlopen(url)
     Counter(filedata.read().strip().split())
     # for key, value in count.most_common(100):
     #     print(key + " - " + str(value))
-    end_time = time.time()
-    print(end_time -startTime)
     return {'token': 'inference finished', 'startTime': int(round(startTime * 1000))}
-
